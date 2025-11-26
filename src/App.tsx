177e20254dc36ad085fe29e4307bb212315bc614
Carlos <devcarlos0112@gmail.com>
2025-11-26 14:28:22 -0300

    Adicionando rotas e layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
 
 export function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+    
   )
 }
---
+++ src/Router.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import { SignIn } from "./pages/auth/SignIn";
+import { Dashboard } from "./pages/app/DashBoard";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+
+// export const router = createBrowserRouter([
+//     { path: "/", element: <Dashboard /> },
+//     { path: "/sign-in", element: <SignIn /> }
+// ])
+
+export function Router() {
+    return(
+        <Routes>
+            <Route path="/" element={<AppLayout />}>
+                <Route path="/" element={<Dashboard />} />
+            </Route>
+            <Route path="/sign-in" element={<AuthLayout />}>
+                <Route path="/sign-in" element={<SignIn />} />
+            </Route>
+            
+        </Routes>
+    );
+}
